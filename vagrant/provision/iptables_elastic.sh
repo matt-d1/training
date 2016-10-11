9d7704f3c39810aa75a7362df374424d83931ae9
matt-d1 <doo.one@gmail.com>
2016-10-11 10:16:46 +0200

    Add files via upload
    updating iptables rules

--- vagrant/provision/iptables_elastic.sh
+++ vagrant/provision/iptables_elastic.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
 
-# iptables remove drop logging to allow ES port first
-        sudo iptables -D INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
-
-# iptables remove drop
-        sudo iptables -D INPUT -j DROP
+# Remove IPTables drop rules
+	./.provision/iptables_remove_drop.sh
 
 # Allow http connection from kibana
 
-#	sudo iptables -A INPUT -s kibana1 -p tcp --dport 9200:9300 -j ACCEPT
-#       sudo iptables -A INPUT -s kibana1 -p udp --dport 9200:9300 -j ACCEPT
-
-# drop all http traffic to elasticsearch port
-# local host will be allowed by localhost rule at begining of rule set
-	sudo iptables -A INPUT -p tcp --dport 9200 -j REJECT
-
-# add http.port Defaults to 9200-9300
-        # sudo iptables -A INPUT -p tcp --dport 9200:9300 -j ACCEPT
-        # sudo iptables -A INPUT -p udp --dport 9200:9300 -j ACCEPT
+# Allow rsyslog > logstash port 9480 for testing until KAFKA setup
+	sudo iptables -A INPUT -p tcp --dport 9480 -j ACCEPT
+	sudo iptables -A INPUT -p udp --dport 9480 -j ACCEPT
 
 # transport.tcp.port - bind port range. Defaults to 9300-9400
         sudo iptables -A INPUT -s ELK1 -p tcp --dport 9300:9400 -j ACCEPT
 	sudo iptables -A INPUT -s ELK1 -p udp --dport 9300:9400 -j ACCEPT
 
 	sudo iptables -A INPUT -s ELK2 -p tcp --dport 9300:9400 -j ACCEPT
-        sudo iptables -A INPUT -s ELK1 -p udp --dport 9300:9400 -j ACCEPT
+        sudo iptables -A INPUT -s ELK2 -p udp --dport 9300:9400 -j ACCEPT
 
-# re-add drop logging
-        sudo iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
-
-# re-add drop
-        sudo iptables -A INPUT -j DROP
-
+# Add IPTables drop rules
+        ./.provision/iptables_drop.sh
